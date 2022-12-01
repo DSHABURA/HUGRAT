a0cf203fb19bdc34747e8fb2ebe9c57bd6ed2b77
RPeterson0487 <peterson4487@live.missouristate.edu>
2022-12-01 01:06:55 -0600

    Camera Fix

--- BeginTranslationPage.py
+++ BeginTranslationPage.py
@@ -15,6 +15,8 @@ from tkinter import filedialog as fd
 
 min_detection_confidence = 0.7
 min_tracking_confidence = 0.7
+
+
 class KeyPointClassifier(object):
     def __init__(
         self,
@@ -47,20 +49,17 @@ class KeyPointClassifier(object):
         return result_index
 
 
-
-
-
-
-
 mp_hands = mp.solutions.hands
 hands = mp_hands.Hands(
     min_detection_confidence=min_detection_confidence,
     min_tracking_confidence=min_tracking_confidence,
-    max_num_hands = 1,
-    static_image_mode = False)
+    max_num_hands=1,
+    static_image_mode=False)
+
 
 def flip_frame(frame):
     return cv.flip(frame, 1)
+
 
 def frame_rgb_to_bgr(frame):
     return cv.cvtColor(frame, cv.COLOR_RGB2BGR)
@@ -68,12 +67,11 @@ def frame_rgb_to_bgr(frame):
 
 class BeginTranslationSidebar(Sidebar):
     def __init__(self, *args,  **kwargs):
-        super().__init__(heading = "Begin Translation",*args, **kwargs)
+        super().__init__(heading="Begin Translation", *args, **kwargs)
 
-        self.add_button(text="Home",command=lambda: self.back())
-        
+        self.add_button(text="Home", command=lambda: self.back())
 
-    def connect_webcam(self,webcam):
+    def connect_webcam(self, webcam):
         self.webcam = webcam
 
     def back(self):
@@ -87,89 +85,83 @@ class BeginTranslationSidebar(Sidebar):
 
 class BeginTranslationContent(Content):
     def __init__(self, *args,  **kwargs):
-            super().__init__(*args, **kwargs)
-            self.grid_rowconfigure(0,weight=1)
-            self.grid_columnconfigure(0,weight=1)
+        super().__init__(*args, **kwargs)
+        self.grid_rowconfigure(0, weight=1)
+        self.grid_columnconfigure(0, weight=1)
 
-            f = fd.askopenfilename(initialdir="./data/", title="Select model file", filetypes=((".tflite", "*.tflite"), ("all files", "*.*")))
+        f = fd.askopenfilename(initialdir="./data/", title="Select model file",
+                               filetypes=((".tflite", "*.tflite"), ("all files", "*.*")))
 
+        self.keypoint_classifier = KeyPointClassifier(model_path=f)
 
-
-            self.keypoint_classifier = KeyPointClassifier(model_path=f)
-            
-
-            self.label_list = []
-            self.keypoint_classifier_labels = []
+        self.label_list = []
+        self.keypoint_classifier_labels = []
 
         # Read labels ###########################################################
-            with open(os.path.dirname(os.path.abspath(f)) + "/labels.csv",encoding='utf-8-sig') as f:
-                self.keypoint_classifier_labels = csv.reader(f)
-                self.keypoint_classifier_labels = [
-                    row[0] for row in self.keypoint_classifier_labels
-                ]
+        with open(os.path.dirname(os.path.abspath(f)) + "/labels.csv", encoding='utf-8-sig') as f:
+            self.keypoint_classifier_labels = csv.reader(f)
+            self.keypoint_classifier_labels = [
+                row[0] for row in self.keypoint_classifier_labels
+            ]
 
+        self.cap = cv.VideoCapture(0, cv.CAP_DSHOW)
 
-            self.cap_width =900
-            self.cap_height = 700
-            self.canvas = Canvas(self, width = self.cap_width , height = self.cap_height)
-            self.canvas.grid(row=0,column=0)
+        self.cap_width = self.cap.get(cv.CAP_PROP_FRAME_WIDTH)
+        self.cap_height = self.cap.get(
+            cv.CAP_PROP_FRAME_HEIGHT)
+        self.canvas = Canvas(self, width=self.cap_width,
+                             height=self.cap_height)
+        self.canvas.grid(row=0, column=0)
 
+        self.cap.set(cv.CAP_PROP_FRAME_WIDTH, self.cap_width)
+        self.cap.set(cv.CAP_PROP_FRAME_HEIGHT, self.cap_height)
+        self.mp_drawing = mp.solutions.drawing_utils
+        self.mp_drawing_styles = mp.solutions.drawing_styles
+        self.mp_hands = mp.solutions.hands
 
-            
-            self.cap = cv.VideoCapture(0, cv.CAP_DSHOW)
-            self.cap.set(cv.CAP_PROP_FRAME_WIDTH, self.cap_width)
-            self.cap.set(cv.CAP_PROP_FRAME_HEIGHT, self.cap_height)
-            self.mp_drawing = mp.solutions.drawing_utils
-            self.mp_drawing_styles = mp.solutions.drawing_styles
-            self.mp_hands = mp.solutions.hands
-
-
-
-            if not self.cap.isOpened():
-                raise IOError("Cannot open webcam")
-            self.update()
+        if not self.cap.isOpened():
+            raise IOError("Cannot open webcam")
+        self.update()
 
     def update(self):
         ret, frame = self.cap.read()
         if ret:
 
             frame = flip_frame(frame)
-            
 
             frame.flags.writeable = False
             results = hands.process(frame)
             frame.flags.writeable = True
 
             if results.multi_hand_landmarks is not None:
-                for hand_landmarks, handedness in zip(results.multi_hand_landmarks,results.multi_handedness):
+                for hand_landmarks, handedness in zip(results.multi_hand_landmarks, results.multi_handedness):
 
                     brect = utils.calc_bounding_rect(frame, hand_landmarks)
-                    landmark_list = utils.calc_landmark_list(frame, hand_landmarks)
+                    landmark_list = utils.calc_landmark_list(
+                        frame, hand_landmarks)
                     # Conversion to relative coordinates / normalized coordinates
                     pre_processed_landmark_list = utils.pre_process_landmark(
                         landmark_list)
-                        
 
                     # Hand sign classification
-                    hand_sign_id = self.keypoint_classifier(pre_processed_landmark_list)
-
+                    hand_sign_id = self.keypoint_classifier(
+                        pre_processed_landmark_list)
 
                     self.mp_drawing.draw_landmarks(
-                            frame,
-                            hand_landmarks,
-                            mp_hands.HAND_CONNECTIONS,
-                            self.mp_drawing_styles.get_default_hand_landmarks_style(),
-                            self.mp_drawing_styles.get_default_hand_connections_style())
+                        frame,
+                        hand_landmarks,
+                        mp_hands.HAND_CONNECTIONS,
+                        self.mp_drawing_styles.get_default_hand_landmarks_style(),
+                        self.mp_drawing_styles.get_default_hand_connections_style())
 
-                    frame = draw_info_text(frame,brect,handedness,self.keypoint_classifier_labels[hand_sign_id])
-
-            
+                    frame = draw_info_text(
+                        frame, brect, handedness, self.keypoint_classifier_labels[hand_sign_id])
 
             frame = frame_rgb_to_bgr(frame)
-            #final render
-            self.photo = ImageTk.PhotoImage(image = Image.fromarray(frame))
+            # final render
+            self.photo = ImageTk.PhotoImage(image=Image.fromarray(frame))
 
-            self.canvas.create_image(0, 0, image = self.photo, anchor = "nw")
+            self.canvas.create_image(0, 0, image=self.photo, anchor="nw")
         self.after(15, self.update)
 
 
@@ -182,6 +174,5 @@ def draw_info_text(image, brect, handedness, hand_sign_text):
         info_text = info_text + ':' + hand_sign_text
     cv.putText(image, info_text, (brect[0] + 5, brect[1] - 4),
                cv.FONT_HERSHEY_SIMPLEX, 0.6, (255, 255, 255), 1, cv.LINE_AA)
-
 
     return image
--- NewDatasetPage.py
+++ NewDatasetPage.py
@@ -12,7 +12,6 @@ import customtkinter as ct
 import random
 
 
-
 current_label = "TESTLABEL"
 label_list = []
 current_results = []
@@ -23,32 +22,32 @@ mp_hands = mp.solutions.hands
 hands = mp_hands.Hands(
     min_detection_confidence=min_detection_confidence,
     min_tracking_confidence=min_tracking_confidence,
-    max_num_hands = 1,
-    static_image_mode = False)
+    max_num_hands=1,
+    static_image_mode=False)
+
 
 class NewDatasetSidebar(Sidebar):
     def __init__(self, *args,  **kwargs):
-        super().__init__(heading = "New Dataset",*args, **kwargs)
-        self.add_button(text="Home",command=lambda: self.go_back())
-        self.add_button(text="Finish",command = lambda: self.begin_training())
-    
+        super().__init__(heading="New Dataset", *args, **kwargs)
+        self.add_button(text="Home", command=lambda: self.go_back())
+        self.add_button(text="Finish", command=lambda: self.begin_training())
+
         self.label = None
         self.label_list = []
 
         self.label_field = ct.CTkEntry(self, width=20)
         self.label_field.grid(row=3, column=0, sticky="ew")
 
-
-        self.error_label = ct.CTkLabel(self, text="", text_color= 'red')
+        self.error_label = ct.CTkLabel(self, text="", text_color='red')
         self.error_label.grid(row=6, column=0, sticky="ew")
 
         self.info_label = ct.CTkLabel(self, text="F1 to capture")
         self.info_label.grid(row=5, column=0, sticky="ew")
 
-        self.capture_button = ct.CTkButton(self,text="Capture", command=lambda: self.capture(self.error_label))
-        self.capture_button.grid(row=4,column=0,sticky="ew")
+        self.capture_button = ct.CTkButton(
+            self, text="Capture", command=lambda: self.capture(self.error_label))
+        self.capture_button.grid(row=4, column=0, sticky="ew")
         self.master.bind('<F1>', lambda event: self.capture(self.error_label))
-
 
     def go_back(self):
         if self.webcam.cap:
@@ -59,35 +58,40 @@ class NewDatasetSidebar(Sidebar):
         if self.webcam.cap:
             self.webcam.cap.release()
         self.master.set_page("begin_training")
-    def connect_webcam(self,webcam):
+
+    def connect_webcam(self, webcam):
         self.webcam = webcam
 
     def capture(self, label):
         self.set_label(self.label_field.get())
         self.webcam.capture_data(label)
-    
+
     def set_label(self, label):
         corrected_label = label.lower()
         if corrected_label and corrected_label not in self.label_list:
             self.label_list.append(corrected_label)
-            #print(self.label_list.index(corrected_label))
+            # print(self.label_list.index(corrected_label))
 
         with open("./data/labels.csv", "w") as f:
             for label in self.label_list:
-                f.write( label+ "\n")
+                f.write(label + "\n")
 
         self.label = corrected_label
         self.webcam.label = corrected_label
         self.webcam.label_list = self.label_list
 
+
 class NewDatasetContent(Content):
-    def frame_rgb_to_bgr(self,frame):
+    def frame_rgb_to_bgr(self, frame):
         return cv.cvtColor(frame, cv.COLOR_RGB2BGR)
-    def frame_bgr_to_rgb(self,frame):
+
+    def frame_bgr_to_rgb(self, frame):
         return cv.cvtColor(frame, cv.COLOR_BGR2RGB)
-    def flip_frame(self,frame):
+
+    def flip_frame(self, frame):
         return cv.flip(frame, 1)
-    def calc_landmark_list(self,image, landmarks):
+
+    def calc_landmark_list(self, image, landmarks):
         image_width, image_height = image.shape[1], image.shape[0]
 
         landmark_point = []
@@ -101,7 +105,8 @@ class NewDatasetContent(Content):
             landmark_point.append([landmark_x, landmark_y])
 
         return landmark_point
-    def calc_relative_landmarks(self,landmark_list):
+
+    def calc_relative_landmarks(self, landmark_list):
         temp_landmark_list = copy.deepcopy(landmark_list)
 
         # Convert to relative coordinates
@@ -127,12 +132,8 @@ class NewDatasetContent(Content):
 
         return temp_landmark_list
 
-
-
-
-
     def add_noise_items(self, landmark_list):
-        #add noise:
+        # add noise:
         noise_landmark_list = copy.deepcopy(landmark_list)
         random.seed()
         for i in range(2, len(noise_landmark_list)):
@@ -147,51 +148,49 @@ class NewDatasetContent(Content):
             writer = csv.writer(f)
             writer.writerow(([label, *relative_landmarks]))
 
-
             for i in range(4):
                 noise_landmark_list = self.add_noise_items(relative_landmarks)
                 writer.writerow(([label, *noise_landmark_list]))
 
         return
+
     def capture_data(self, label):
         if self.label:
-            self.logging_csv(self.label_list.index(self.label), self.relative_list)
+            self.logging_csv(self.label_list.index(
+                self.label), self.relative_list)
         else:
 
             label.configure(text='Name required for capture')
-   
+
     def __init__(self, *args,  **kwargs):
         super().__init__(*args, **kwargs)
 
         self.label = None
         self.label_list = []
-        self.grid_rowconfigure(0,weight=1)
-        self.grid_columnconfigure(0,weight=1)
+        self.grid_rowconfigure(0, weight=1)
+        self.grid_columnconfigure(0, weight=1)
 
-
-        #clear training data
+        # clear training data
         with open("./data/training_data.csv", "w") as f:
             f.write("")
-        #clear labels
+        # clear labels
         with open("./data/labels.csv", "w") as f:
             f.write("")
-            
 
-        self.cap_width =900
-        self.cap_height = 700
-        self.canvas = Canvas(self, width = self.cap_width , height = self.cap_height)
-        self.canvas.grid(row=0,column=0)
-
-
-        
         self.cap = cv.VideoCapture(0, cv.CAP_DSHOW)
+
+        self.cap_width = self.cap.get(cv.CAP_PROP_FRAME_WIDTH)
+        self.cap_height = self.cap.get(
+            cv.CAP_PROP_FRAME_HEIGHT)
+        self.canvas = Canvas(self, width=self.cap_width,
+                             height=self.cap_height)
+        self.canvas.grid(row=0, column=0)
+
         self.cap.set(cv.CAP_PROP_FRAME_WIDTH, self.cap_width)
         self.cap.set(cv.CAP_PROP_FRAME_HEIGHT, self.cap_height)
         self.mp_drawing = mp.solutions.drawing_utils
         self.mp_drawing_styles = mp.solutions.drawing_styles
         self.mp_hands = mp.solutions.hands
-
-
 
         if not self.cap.isOpened():
             raise IOError("Cannot open webcam")
@@ -201,42 +200,40 @@ class NewDatasetContent(Content):
         ret, frame = self.cap.read()
         if ret:
 
-            #frame pre-processing
+            # frame pre-processing
             frame = self.frame_rgb_to_bgr(frame)
             frame = self.flip_frame(frame)
 
-            #hand detection
+            # hand detection
             frame = self.frame_bgr_to_rgb(frame)
             frame.flags.writeable = False
             results = hands.process(frame)
             frame.flags.writeable = True
 
             if results.multi_hand_landmarks is not None:
-                for hand_landmarks, handedness in zip(results.multi_hand_landmarks,results.multi_handedness):
-                    
-                    
-                    #Convert from 0-1 to pixel coordinates (step 1)
-                    landmark_list = self.calc_landmark_list(frame, hand_landmarks)
-                    #convert to relative coordinates (step 2) (relative to wrist, or id 0)
-                    #flattent to 1d array (step 3)
-                    #normalize (step 4)
-                    self.relative_list = self.calc_relative_landmarks(landmark_list)
+                for hand_landmarks, handedness in zip(results.multi_hand_landmarks, results.multi_handedness):
 
+                    # Convert from 0-1 to pixel coordinates (step 1)
+                    landmark_list = self.calc_landmark_list(
+                        frame, hand_landmarks)
+                    # convert to relative coordinates (step 2) (relative to wrist, or id 0)
+                    # flattent to 1d array (step 3)
+                    # normalize (step 4)
+                    self.relative_list = self.calc_relative_landmarks(
+                        landmark_list)
 
-                    #if the capture button is pressed, it will call capture_frame
+                    # if the capture button is pressed, it will call capture_frame
 
                     self.mp_drawing.draw_landmarks(
-                            frame,
-                            hand_landmarks,
-                            mp_hands.HAND_CONNECTIONS,
-                            self.mp_drawing_styles.get_default_hand_landmarks_style(),
-                            self.mp_drawing_styles.get_default_hand_connections_style())
-
-            
+                        frame,
+                        hand_landmarks,
+                        mp_hands.HAND_CONNECTIONS,
+                        self.mp_drawing_styles.get_default_hand_landmarks_style(),
+                        self.mp_drawing_styles.get_default_hand_connections_style())
 
             frame = self.frame_rgb_to_bgr(frame)
-            #final render
-            self.photo = ImageTk.PhotoImage(image = Image.fromarray(frame))
+            # final render
+            self.photo = ImageTk.PhotoImage(image=Image.fromarray(frame))
 
-            self.canvas.create_image(0, 0, image = self.photo, anchor = "nw")
+            self.canvas.create_image(0, 0, image=self.photo, anchor="nw")
         self.after(15, self.update)
